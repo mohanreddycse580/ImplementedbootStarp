9ea3c3e3c22859dc05114789578017dbbf828d78
mohanreddycse580 <mohanreddycse580@gmail.com>
2019-01-10 11:34:21 +0530

    IimplementedBootStarp
    implementedBootStarp

--- src/header/Header.js
+++ src/header/Header.js
@@ -12,7 +12,6 @@ import {
   DropdownMenu,
   DropdownItem
 } from "reactstrap";
-import AppBar from "@material-ui/core/AppBar";
 
 class Header extends React.Component {
   constructor(props) {
@@ -30,27 +29,40 @@ class Header extends React.Component {
   }
 
   render() {
+    const NavbarStyle = {
+      "background-color": "rgb(0, 174, 255)"
+    };
+    const textColor = {
+      color: "white",
+      "text-align": "center"
+    };
     return (
       <div>
-        <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">Home</NavbarBrand>
+        <Navbar light expand="md" style={NavbarStyle}>
+          <NavbarBrand style={textColor} href="/">
+            Home
+          </NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
               <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
+                <DropdownToggle style={textColor} nav caret>
                   USERS
                 </DropdownToggle>
-                <DropdownMenu right>
+                <DropdownMenu>
                   <DropdownItem href="/add">ADD</DropdownItem>
                   <DropdownItem href="/delete">VIEW</DropdownItem>
                 </DropdownMenu>
               </UncontrolledDropdown>
               <NavItem>
-                <NavLink href="/claim">CLAIMS</NavLink>
+                <NavLink style={textColor} href="/claim">
+                  CLAIMS
+                </NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="/insurence">INSURENCE PAYERS</NavLink>
+                <NavLink style={textColor} href="/insurence">
+                  INSURENCE PAYERS
+                </NavLink>
               </NavItem>
             </Nav>
           </Collapse>
